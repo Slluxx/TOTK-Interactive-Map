b336de70098c07d8b5582f431b1c596f97dd75a2
Slluxx <Slluxx@users.noreply.github.com>
2023-05-11 00:41:17 +0200

    Make marker movable

--- js/markers/mouseclick.js
+++ js/markers/mouseclick.js
@@ -1,24 +1,33 @@
 function setMouseClickMarkerEvent() {
     // set markers on click events in the map
     map.on('click', function (event) {
-
         if (window.mouseMarker != null) window.mouseMarker.remove()
+        generateMarker(event);
+    })
+}
 
-        var zoom = window.map.getZoom();
-        var center = window.map.getCenter();
-        var pxcoords = window.rc.project([center.lat, center.lng])
+function generateMarker(event){
+    
 
-        // any position in leaflet needs to be projected to obtain the image coordinates
-        var coords = rc.project(event.latlng)
-        window.mouseMarker = L.marker(rc.unproject(coords), {
-            draggable: true,
-            autoPan: true
-        }).addTo(map)
+    var zoom = window.map.getZoom();
+    var center = window.map.getCenter();
+    var pxcoords = window.rc.project([center.lat, center.lng])
 
-        let html = `<a href="${location.protocol}//${location.host}${location.pathname}?z=${zoom}&x=${coords.x}&y=${coords.y}">Marker</a></br>
-        [${Math.floor(coords.x)},${Math.floor(coords.y)}]
-        `
+    // any position in leaflet needs to be projected to obtain the image coordinates
+    var coords = rc.project(event.latlng || event.target._latlng)
+    window.mouseMarker = L.marker(rc.unproject(coords), {
+        draggable: true,
+        autoPan: true
+    }).addTo(map)
 
-        window.mouseMarker.bindPopup(html).openPopup()
+    let html = `<a href="${location.protocol}//${location.host}${location.pathname}?z=${zoom}&x=${coords.x}&y=${coords.y}">Marker</a></br>
+    [${Math.floor(coords.x)},${Math.floor(coords.y)}]
+    `
+
+    window.mouseMarker.bindPopup(html).openPopup()
+
+    window.mouseMarker.on("dragend", function(event){
+        if (window.mouseMarker != null) window.mouseMarker.remove()
+        generateMarker(event);
     })
 }
