f76233cf5c74901e5a4ed49eface8a5703c8de77
Slluxx <Slluxx@users.noreply.github.com>
2023-05-10 23:21:57 +0200

    Update mouseclick.js

--- js/markers/mouseclick.js
+++ js/markers/mouseclick.js
@@ -4,12 +4,16 @@ function setMouseClickMarkerEvent() {
 
         if (window.mouseMarker != null) window.mouseMarker.remove()
 
+        var zoom = window.map.getZoom();
+        var center = window.map.getCenter();
+        var pxcoords = window.rc.project([center.lat, center.lng])
+
         // any position in leaflet needs to be projected to obtain the image coordinates
         var coords = rc.project(event.latlng)
         window.mouseMarker = L.marker(rc.unproject(coords), {
             draggable: true,
             autoPan: true
         }).addTo(map)
-        window.mouseMarker.bindPopup('[' + Math.floor(coords.x) + ',' + Math.floor(coords.y) + ']').openPopup()
+        window.mouseMarker.bindPopup("<a href="+location.protocol + '//' + location.host + location.pathname+"?z="+zoom+"&x="+pxcoords.x+"&y="+pxcoords.y+">Marker</a>").openPopup()
     })
 }
